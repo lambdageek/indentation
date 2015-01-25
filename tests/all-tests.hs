58e39c1babea6f97f352aad772682ca9f6d6e5bc
Aleksey Kliger <aleksey@lambdageek.org>
2015-01-25 14:31:16 -0500

    Add (parsec) testcase

---
+++ tests/all-tests.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE CPP #-}
+module Main where
+
+import Test.Tasty (defaultMain, testGroup)
+#if defined(ENABLE_PARSEC_TESTS)
+import qualified ParensParsec 
+#endif
+
+#if defined(ENABLE_TRIFECTA_TESTS)
+#endif
+
+main =
+  defaultMain $ testGroup "All tests" $
+  [
+#if defined(ENABLE_PARSEC_TESTS)
+    ParensParsec.allTests
+#endif
+  ]
+  ++
+  [
+#if defined(ENABLE_TRIFECTA_TESTS)
+#endif
+  ]
